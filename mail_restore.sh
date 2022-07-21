c35a737f9f4aad8dfbdf949c47fb4489d7387248
ducarpes <52746293+ducarpes@users.noreply.github.com>
2022-07-21 20:31:20 -0300

    Update mail_restore.sh

--- mail_restore.sh
+++ mail_restore.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
-# 
+#
 # restore-mail.sh - Realiza restauração de e-mails em arquivo de backup de cpanel.
 # 
 # Autor:      Eduardo C. Souza 
@@ -37,6 +37,7 @@ AMARELO="\033[33;1m"
 COLOR_OFF="\033[0m"
 RED_SINAL="[$VERMELHO ! $COLOR_OFF]"
 GREEN_SINAL="[$VERDE ! $COLOR_OFF]"
+YELLOW_SINAL="[$AMARELO ! $COLOR_OFF]"
 
 #CONTADORES E CHAVES
 EMAIL_CHAVE=0
@@ -52,13 +53,14 @@ ajuda(){
 
 echo -e "
 $VERMELHO[AJUDA]$COLOR_OFF
-
-1 - Execute o comando detro do mesmo diretório que arquivo que deseja restaurar;
-2 - Não Execute o script definindo o arquivo de backup utilizando o nome de diretório absoluto
+ 
+1 - Não utilize o caminho absoluto para designar o arquivo de backup;
+2 - Execute o comando detro do mesmo diretório que arquivo que deseja restaurar;
+3 - O usuário do backup deverá ser o mesmo usuário do Cpanel.
 
 $VERMELHO[SINTAXE]$COLOR_OFF
 
-./restore-mail.sh $AMARELO <ARQUIVO_DE_BACKUP> <DOMÍNIO / -a> $COLOR_OFF
+./restore-mail.sh $AMARELO <ARQUIVO_DE_BACKUP> <DOMÍNIO | -a> $COLOR_OFF
 
 $VERDE [EXEMPLO]: $COLOR_OFF ./restore-mail.sh $AMARELO backup-6.7.2022_17-02-55_exemplo.tar.gz exemplo.com.br $COLOR_OFF
 
@@ -76,6 +78,13 @@ exit
 # ------------------------------------------------------------------------ # 
 
 # ------------------------------- TESTES ----------------------------------------- # 
+
+if [ "$PWD" = "/" ]
+    then 
+        echo -e "$RED_SINAL -$AMARELO Não é possível executar este script diretamente dentro do diretório raiz (/).$COLOR_OFF"
+        echo -e "$YELLOW_SINAL - Execute o Script em outro diretório."
+        exit 1
+fi
 
 #VERIFICAR SE A AJUDA FOI CHAMADA (--help)
 if [ "$1" = "--help" ] || [ "$2" = "--help" ]; 
@@ -115,7 +124,8 @@ if [ -f $PWD/$1 ];
                 ;;
             
             *)
-                echo "Arquivo inválido"
+                echo -e "$RED_SINAL -$AMARELO O Arquivo $VERMELHO$1$AMARELO é inválido$COLOR_OFF!"
+                echo -e "$YELLOW_SINAL - Certifique-se de utilizar um backup completo de Cpanel válido!"
                 exit 1
                 ;;  
         esac
@@ -123,7 +133,9 @@ if [ -f $PWD/$1 ];
         #VERIFICAR SE O USUÁRIO É VÁLIDO (EVITAR QUE O USUÁRIO SEJA O ROOT) (EVITAR CONFLITO COM ARQUIVOS DE MESMO NOME)
         if [ -f $PWD/$BACKUP_NAME ]
         then
-            echo -e "$RED_SINAL -$AMARELO Já existe um arquivo chamado $BACKUP_NAME neste diretório. Não é possível realizar a extração! Remova ou renomeie o arquivo existe!  $COLOR_OFF"
+            echo -e "$RED_SINAL -$AMARELO Já existe um diretório chamado $VERMELHO$BACKUP_NAME$AMARELO dentro deste diretório atual. Não é possível realizar a extração!$COLOR_OFF"
+            echo -e "$YELLOW_SINAL - Se possível Remova ou renomeie o diretório existente ou mova o backup para outro diretório!"
+            #Atualização para o futuro: implementar utilização de um diretório temporário para extração dos arquivos
             exit 1
         fi
         if [ $USUARIO = "root" ]
@@ -143,10 +155,12 @@ if [ $? -eq 0 ]; then
     if [ ! -d "$HOME_USUARIO" ]
     then
         echo -e "$RED_SINAL -$AMARELO O usuário $VERMELHO$USUARIO$AMARELO não foi encontrado neste servidor! $COLOR_OFF"
+        echo -e "$YELLOW_SINAL - Certifique-se que o usuário $VERMELHO$USUARIO$COLOR_OFF possui uma conta de CPANEL no servidor $HOSTNAME"
         exit 1
     fi
 else
     echo -e "$RED_SINAL -$AMARELO O usuário $VERMELHO$USUARIO$AMARELO não foi encontrado neste servidor! $COLOR_OFF"
+    echo -e "$YELLOW_SINAL - Certifique-se que o usuário $VERMELHO$USUARIO$COLOR_OFF possui uma conta de CPANEL no servidor $VERMELHO$HOSTNAME$COLOR_OFF"
     exit 1
 fi
 
@@ -157,26 +171,45 @@ fi
 
 #REALIZA A RESTAURAÇÂO DE TODA A PASTA /MAIL CASO A OPÇÃO -a SEJA UTILIZADA E FINALIZA O SCRIPT
 if [ $COMPLETO_CHAVE -eq 1 ]; then
-
-    echo -e "$GREEN_SINAL - O Usuário $VERDE$USUARIO$COLOR_OFF existe neste servidor!"
-    echo -e "$GREEN_SINAL - O backup de todos os e-mails será restaurado:"
-    echo -e "$GREEN_SINAL - Extraindo conteúdo dos e-mails..."
-    tar -xf $1 $BACKUP_NAME/homedir/mail --warning=no-timestamp 2>/dev/null
-    tar -xf $1 $BACKUP_NAME/homedir/etc --warning=no-timestamp 2>/dev/null
-    if [ $? -eq 0 ]
-    then
-        echo -e "$GREEN_SINAL - Restaurando e-mails com Rsync..."
-        if [ ! -d $HOME_USUARIO/mail ]
+    read -p "$(echo -e "$YELLOW_SINAL Este procedimento realiza a restauração de backup completo de e-mails. Deseja prosseguir? (s/n):")" RESTORE_ALL_INPUT
+    if [ "$RESTORE_ALL_INPUT" = "s" ]; then
+        echo -e "$GREEN_SINAL - O Usuário $VERDE$USUARIO$COLOR_OFF existe neste servidor!"
+        echo -e "$GREEN_SINAL - O backup de todos os e-mails será restaurado:"
+        echo -e "$GREEN_SINAL - Extraindo conteúdo dos e-mails..."
+        tar -xf $1 $BACKUP_NAME/homedir/mail --warning=no-timestamp 2>/dev/null
+        tar -xf $1 $BACKUP_NAME/homedir/etc --warning=no-timestamp 2>/dev/null
+        if [ $? -eq 0 ]
         then
-            mkdir -p $HOME_USUARIO/mail/
+            echo -e "$GREEN_SINAL - Restaurando e-mails com Rsync..."
+            if [ ! -d $HOME_USUARIO/mail ]
+            then
+                mkdir -p $HOME_USUARIO/mail/
+                
+            fi
+            if [ ! -d $HOME_USUARIO/etc ]
+            then 
+                mkdir -p $HOME_USUARIO/etc/
+            fi
+
+            #BACKUP /ETC
+            for diretorio in $(ls -l $HOME_USUARIO/etc/ | grep '^d' | awk -F" " {'print$9'}); do cp $HOME_USUARIO/etc/$diretorio/shadow $HOME_USUARIO/etc/$diretorio/shadow.bkp; done;
+            for diretorio in $(ls -l $HOME_USUARIO/etc/ | grep '^d' | awk -F" " {'print$9'}); do cp $HOME_USUARIO/etc/$diretorio/passwd $HOME_USUARIO/etc/$diretorio/passwd.bkp; done;
+            tar -czf $HOME_USUARIO/etc/etc-bkp.$DATA_BACKUP.$HORA_BACKUP.tar.gz $HOME_USUARIO/etc/ -P --warning=no-timestamp 2>/dev/null
+            #RESTORE
+            rsync -qzarhP $BACKUP_NAME/homedir/etc/* $HOME_USUARIO/etc/
+            rsync -qzarhP $BACKUP_NAME/homedir/mail/* $HOME_USUARIO/mail/
+            for diretorio in $(ls -l $HOME_USUARIO/etc/ | grep '^d' | awk -F" " {'print$9'}); do cat $HOME_USUARIO/etc/$diretorio/shadow $HOME_USUARIO/etc/$diretorio/shadow.bkp | sort -u -o $HOME_USUARIO/etc/$diretorio/shadow; done 2>/dev/null
+            for diretorio in $(ls -l $HOME_USUARIO/etc/ | grep '^d' | awk -F" " {'print$9'}); do cat $HOME_USUARIO/etc/$diretorio/passwd $HOME_USUARIO/etc/$diretorio/passwd.bkp | sort -u -o $HOME_USUARIO/etc/$diretorio/passwd; done 2>/dev/null
+
+            echo -e "$GREEN_SINAL - Todos os E-mails disponíveis neste backup foram restaurados com sucesso."
+            rm -rf $PWD/$BACKUP_NAME
+            exit 0
+        else
+            echo -e "$RED_SINAL -$AMARELO Arquivo de Backup selecionado pode estar corrompido ou possui formato inválido para esta restauração.$COLOR_OFF"
+            exit 1
         fi
-        rsync -qzarhP $BACKUP_NAME/homedir/etc/* $HOME_USUARIO/etc/
-        rsync -qzarhP $BACKUP_NAME/homedir/mail/* $HOME_USUARIO/mail/
-        echo -e "$GREEN_SINAL - E-mails restaurados com sucesso."
-        rm -rf $PWD/$BACKUP_NAME
-        exit 0
     else
-        echo -e "$RED_SINAL -$AMARELO Arquivo de Backup selecionado está corrompido ou possui formato inválido para esta restauração.$COLOR_OFF"
+        echo -e "$GREEN_SINAL - A restauração completa não será executada"
         exit 1
     fi
 fi
@@ -188,7 +221,7 @@ fi
 tar -xf $1 $BACKUP_NAME/cp/$USUARIO --warning=no-timestamp --ignore-command-error;
 if [ $? -ne 0 ]
 then
-    echo -e "$RED_SINAL -$AMARELO Arquivo de Backup selecionado está corrompido ou possui formato inválido para esta restauração.$COLOR_OFF"
+    echo -e "$RED_SINAL -$AMARELO Arquivo de Backup selecionado pode estar corrompido ou possui formato inválido para esta restauração.$COLOR_OFF"
     exit 1
 else
     if [ -d $BACKUP_NAME/cp/ ];
@@ -222,12 +255,13 @@ fi
     
 #CRIA FUNÇÃO PARA LISTAR OS EMAILS DO DOMÍNIO ESCOLHIDO E SOLICITA QUE UM DELES SEJA SELECIONADO PARA RESTAURAÇÃO:
 restore(){
+echo -e "$GREEN_SINAL - Os seguintes E-mails foram localizados e estão disponíveis para restauração:"    
 for LINE in $EMAIL_USERS;
     do echo -e $AMARELO"E-mail localizado: $VERDE $LINE@$DOMINIO_INPUT $COLOR_OFF";
 done;
 
-echo -e "$GREEN_SINAL - Selecione um dos e-mails listados para prosseguir com a restauração da conta"
-read -p "Insira o nome do arquivo: " EMAIL_INPUT
+echo -e "$GREEN_SINAL - Selecione um dos E-mails listados para prosseguir com a restauração da conta."
+read -p "Insira o E-mail que deseja restaurar: " EMAIL_INPUT
 
 #VERIFICA SE O INPUT É UM DOS EMAILS LISTADOS E ATIVE UM SWITCH PARA PERMITIR O PROSSEGUIMENTO:
 for LINE in $EMAIL_USERS
